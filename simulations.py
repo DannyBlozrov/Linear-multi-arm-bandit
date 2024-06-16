b328acc79c14fe8ebb474ff8173cd2ad4b039c11
DannyBlozrov <104273555+DannyBlozrov@users.noreply.github.com>
2024-06-16 10:33:34 +0300

    Update simulations.py

--- simulations.py
+++ simulations.py
@@ -69,7 +69,8 @@ def g_optimal(arm_matrix:np.ndarray,indexes:list):
         :return: the best arm in given distribution using a_i^T V(pi)^-1 a as the mahalobanius norm
         """
         V_pi:np.ndarray = V(pi)
-        print(f"V_pi = {V_pi}")
+        # print(f"Det = {np.linalg.det(V_pi)}")
+        # print(f"V_pi = \n {V_pi}")
         return np.max([(arm_matrix[:,i].reshape((d,1))).T@np.linalg.inv(V_pi).reshape((d,d))@(arm_matrix[:,i].reshape((d,1))) for i in indexes])
 
     constraints = lambda v : np.sum(v) - 1 #sum of probabilities must equal 1
@@ -78,10 +79,10 @@ def g_optimal(arm_matrix:np.ndarray,indexes:list):
     res = optimize.minimize(fun=g,bounds=[(0,1) for i in indexes],constraints = constraints_dict,x0 = pi_0)
     val = res['fun']
     pi = res['x']
-    solver = res['status']
+    solver = res['message']
     return pi,solver
 
-def simulate_fixed_budget(k=5, d=4, T=400,mean=0,sigma = 1, num_trials = 1):
+def simulate_fixed_budget(k=50, d=20, T=400,mean=0,sigma = 1, num_trials = 1):
     """
 
     :param k:  number of samples(arms)
@@ -92,8 +93,9 @@ def simulate_fixed_budget(k=5, d=4, T=400,mean=0,sigma = 1, num_trials = 1):
     :param num_trials : number of trials to average the error
     :return:the optimal arm from the list of arms generated
     """
-    arm_vectors, theta_star = generate_linear_bandit_instance(k,d)
-    curr_arms = arm_vectors
+    original_arm_vectors, original_theta_star = generate_linear_bandit_instance(k,d)
+    print(f"Arm Vectors =\n {original_arm_vectors}")
+    curr_arms = original_arm_vectors
     curr_indexes = list(range(k))
     logd = math.ceil(math.log2(d))
     print(f"logd={logd}")
@@ -101,23 +103,43 @@ def simulate_fixed_budget(k=5, d=4, T=400,mean=0,sigma = 1, num_trials = 1):
     new_dim = d
     m = (T - np.min([k , (d*(d+1)/2)]) -sum([d/(2**r) for r in range(1,logd)]))/logd
 
-    for r in range(1,logd):
+    for r in range(1,logd+1):
         print(curr_arms.shape)
         new_dim = np.linalg.matrix_rank(curr_arms)
         print(f"new dim ={new_dim}")
         if (new_dim != curr_dim):
-            pass
-            #need to find a matrix B whose columns form a orthonormal basis of the subspace
-            #spanned by {ar−1(i) : i ∈ Ar−1}.
-
+            (B,R) = np.linalg.qr(curr_arms,mode='reduced')
+            print("SDSDSDSDS")
+            print(f"k={k},d={d},B shape = {B.shape}")
+            print(np.linalg.matrix_rank(B))
         if r == 1:
             pi,solver = g_optimal(curr_arms,curr_indexes)
-            print(f"PI = {pi},solver={solver},r={r}") # vector of probabilities
+            print(f"PI = {pi}r={r}") # vector of probabilities
         elif r != 1:
             pi,solver = g_optimal(curr_arms,curr_indexes)
-            print(f"PI = {pi},solver={solver},r={r}")
-        T_rs = np.asarray([np.ceil(pi[i]*m for i in range(len(pi)))])
-        Tr = np.sum(T_rs)
+            print(f"PI = {pi},r={r}")
+
+        T_r_array = np.zeros((k))
+        for i in curr_indexes:
+            T_r_array[i] = np.ceil(pi[i]*m)
+        Tr = np.sum(T_r_array)
+        print(f"T_rs = {T_r_array}")
+        print(f"Tr = {Tr}")
+        V_r = sum(T_r_array[i]*(curr_arms[i]@curr_arms[i].T) for i in curr_indexes)
+        print(f"V_r ={V_r}")
+        newsum = np.zeros((curr_dim,1)) #from 19 in alg
+        for idx in curr_indexes:
+            for j in range(T_r_array[idx]):
+                X_t = get_reward(original_theta_star,original_arm_vectors[idx])
+                newsum += curr_arms[idx]*X_t
+        Theta_r = np.linalg.inv(V_r)@newsum #this is Theta_r as in phase 19
+        expected_rewards = np.zeros(k)
+        for idx in curr_indexes:
+            expected_rewards[idx] = np.dot(Theta_r,curr_arms[idx])
+
+
+
+
 
 
 
