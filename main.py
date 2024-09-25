c3236497ca1e413e460b52b5e8108568d920f7be
deaneli13 <74005943+deaneli13@users.noreply.github.com>
2024-09-25 16:37:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,21 @@ import numpy as np
 from bandit_mod import simulate_modified
 from bandit_vanillam2 import simulate_vanilla
 from functions import make_plots
+from functions import calculate_kl_divergence_with_uniform
 
 
 # Prompt user for input values
 k = int(input("Enter the number of arms (k) [default is 50]: ") or 50)
 d = int(input("Enter the dimension of each arm (d) [default is 5]: ") or 5)
 T = int(input("Enter the budget for the simulation (T) [default is 400]: ") or 400)
-sim_num = int(input("Enter the number of simulations (T) [default is 100]: ") or 100)
+sim_num = int(input("Enter the number of simulations (Sim) [default is 100]: ") or 100)
 
 mod_correct_counter = 0
 vanilla_correct_counter = 0
 armpull_mod = 0
 armpull_vanilla = 0
+KLdiv_mod = 0
+KLdiv_vanilla = 0
 for i in range(sim_num):
     # Run the simulation with user-defined parameters
     plot_data_mod, original_theta_star, original_arm_vectors,total_appearances_mod,is_correct_mod = simulate_modified(k=k, d=d, T=T)
@@ -22,6 +25,8 @@ for i in range(sim_num):
     vanilla_correct_counter+=is_correct_vanilla
     armpull_mod+=total_appearances_mod
     armpull_vanilla+=total_appearances_vanilla
+    KLdiv_mod +=  calculate_kl_divergence_with_uniform(plot_data_mod)
+    KLdiv_vanilla +=  calculate_kl_divergence_with_uniform(plot_data_vanilla)
 
 #average of armpulls in each simulation
 avg_armpull_mod = armpull_mod / sim_num
@@ -31,6 +36,10 @@ avg_armpull_vanilla = armpull_vanilla / sim_num
 error_prob_mod = 1 - (mod_correct_counter / sim_num)
 error_prob_vanilla = 1 - (vanilla_correct_counter / sim_num)
 
+#average of KL Divergence in each simulation
+avg_KLdiv_mod = KLdiv_mod / sim_num
+avg_KLdiv_vanilla = KLdiv_vanilla / sim_num
+
 
 # Print avg armpulls
 print("avg armpulls (modified):", avg_armpull_mod)
@@ -39,6 +48,11 @@ print("avg armpulls (vanilla):", avg_armpull_vanilla)
 # Print Avg error probability 
 print("Avg error probability (modified):", error_prob_mod)
 print("Avg error probability (vanilla):", error_prob_vanilla)
+
+
+# Print Avg KL Divergence 
+print("Avg KL Divergence (modified):", avg_KLdiv_mod)
+print("Avg KL Divergence (vanilla):", avg_KLdiv_vanilla)
 
 
 # Generate the plots from the simulation data
